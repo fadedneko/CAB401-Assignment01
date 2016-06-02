5ea994b1bdeae3b07f46d34bf831b11a80e865b4
olivergiess <oli_g_93@hotmail.com>
2016-06-03 09:29:31 +1000

    Initial commit

---
+++ Modified/WpfApplication1/timefreq.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class timefreq
+    {
+        public float[][] timeFreqData;
+        public int wSamp;
+        public Complex[] twiddles;
+
+        public timefreq(float[] x, int windowSamp)
+        {
+            int ii;
+            double pi = 3.14159265;
+            Complex i = Complex.ImaginaryOne;
+            this.wSamp = windowSamp;
+            twiddles = new Complex[wSamp];
+            for (ii = 0; ii < wSamp; ii++)
+            {
+                double a = 2 * pi * ii / (double)wSamp;
+                twiddles[ii] = Complex.Pow(Complex.Exp(-i), (float)a);
+            }
+
+            timeFreqData = new float[wSamp/2][];
+
+            int nearest = (int)Math.Ceiling((double)x.Length / (double)wSamp);
+            nearest = nearest * wSamp;
+
+            Complex[] compX = new Complex[nearest];
+            for (int kk = 0; kk < nearest; kk++)
+            {
+                if (kk < x.Length)
+                {
+                    compX[kk] = x[kk];
+                }
+                else
+                {
+                    compX[kk] = Complex.Zero;
+                }
+            }
+
+
+            int cols = 2 * nearest /wSamp;
+
+            for (int jj = 0; jj < wSamp / 2; jj++)
+            {
+                timeFreqData[jj] = new float[cols];
+            }
+
+            timeFreqData = stft(compX, wSamp);
+	
+        }
+
+        float[][] stft(Complex[] x, int wSamp)
+        {
+            int ii = 0;
+            int jj = 0;
+            int kk = 0;
+            int ll = 0;
+            int N = x.Length;
+            float fftMax = 0;
+            
+            float[][] Y = new float[wSamp / 2][];
+
+            for (ll = 0; ll < wSamp / 2; ll++)
+            {
+                Y[ll] = new float[2 * (int)Math.Floor((double)N / (double)wSamp)];
+            }
+            
+            Complex[] temp = new Complex[wSamp];
+            Complex[] tempFFT = new Complex[wSamp];
+
+            for (ii = 0; ii < 2 * Math.Floor((double)N / (double)wSamp) - 1; ii++)
+            {
+
+                for (jj = 0; jj < wSamp; jj++)
+                {
+                    temp[jj] = x[ii * (wSamp / 2) + jj];
+                }
+
+                tempFFT = fft(temp);
+
+                for (kk = 0; kk < wSamp / 2; kk++)
+                {
+                    Y[kk][ii] = (float)Complex.Abs(tempFFT[kk]);
+
+                    if (Y[kk][ii] > fftMax)
+                    {
+                        fftMax = Y[kk][ii];
+                    }
+                }
+
+
+            }
+
+            for (ii = 0; ii < 2 * Math.Floor((double)N / (double)wSamp) - 1; ii++)
+            {
+                for (kk = 0; kk < wSamp / 2; kk++)
+                {
+                    Y[kk][ii] /= fftMax;
+                }
+            }
+
+            return Y;
+        }
+
+        public static int BitReverse(int n, int bits)
+        {
+            int reversedN = n;
+            int count = bits - 1;
+
+            n >>= 1;
+            while (n > 0)
+            {
+                reversedN = (reversedN << 1) | (n & 1);
+                count--;
+                n >>= 1;
+            }
+
+            return ((reversedN << count) & ((1 << bits) - 1));
+        }
+
+        Complex[] fft(Complex[] x)
+        {
+            int length = x.Length;
+            Complex[] output = new Complex[length];
+
+            int bits = (int)Math.Log(length, 2);
+            for (int j = 0; j < length; j++)
+            {
+                int swapPos = BitReverse(j, bits);
+                output[j] = x[swapPos];
+            }
+            for (int N = 2; N <= length; N <<= 1)
+            {
+                for (int i = 0; i < length; i += N)
+                {
+                    for (int k = 0; k < N / 2; k++)
+                    {
+                        int evenIndex = i + k;
+                        int oddIndex = i + k + (N / 2);
+                        var even = output[evenIndex];
+                        var odd = output[oddIndex];
+                        output[evenIndex] = even + odd * twiddles[k * (length / N)];
+                        output[oddIndex] = even + odd * twiddles[(k + (N / 2)) * (length / N)];
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
